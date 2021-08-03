cf2c708e2f56183549e8c9fb3738621301c38e52
Jenny Juyoung <katsubear@DESKTOP-OHDJH7F>
2021-08-02 22:39:09 -0400

    Check how to use GroupOfCards object

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -6,12 +6,12 @@
 package ca.sheridancollege.project;
 
 /**
- * A class to be used as the base Card class for the project. 
- * Must be general enough to be instantiated for any Card game. 
- * Students wishing to add to the code should remember to add 
- * themselves as a modifier.
+ * A class to be used as the base Card class for the project. Must be general
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
  *
- * @author Juyoung Jung
+ * @author dancye
+ * @author Paul Bonenfant Jan 2020
  */
 public abstract class Card {
     //default modifier for child classes
@@ -19,8 +19,8 @@ public abstract class Card {
     /**
      * Students should implement this method for their specific children classes
      *
-     * @return a String representation of a card. Could be an UNO card, 
-     * a regular playing card etc.
+     * @return a String representation of a card. Could be an UNO card, a
+     * regular playing card etc.
      */
     @Override
     public abstract String toString();
--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -5,6 +5,7 @@
  */
 package ca.sheridancollege.project;
 
+import ca.sheridancollege.project.WarCard.Deck;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -18,11 +19,16 @@ import java.util.Collections;
 public class GroupOfCards {
 
     //The group of cards, stored in an ArrayList
-    private ArrayList<Card> cards;
+    private ArrayList<WarCard> cards;
     private int size;//the size of the grouping
 
-    public GroupOfCards(int size) {
-        this.size = size;
+    
+    /**
+     * 
+     * @param size 
+     */
+    public GroupOfCards() {
+        cards = new ArrayList<WarCard>();
     }
 
     /**
@@ -30,7 +36,7 @@ public class GroupOfCards {
      *
      * @return the group of cards.
      */
-    public ArrayList<Card> getCards() {
+    public ArrayList<WarCard> getCards() {
         return cards;
     }
 
@@ -42,7 +48,7 @@ public class GroupOfCards {
      * @return the size of the group of cards
      */
     public int getSize() {
-        return size;
+        return cards == null ? 0 : getCards().size();
     }
 
     /**
@@ -51,5 +57,63 @@ public class GroupOfCards {
     public void setSize(int size) {
         this.size = size;
     }
+    
+
+    // This main method is for testing GroupOfCards object
+    // NEED TO REMOVE
+    public static void main(String[] args) {
+        
+        Deck[] deck = Deck.values(); // using Deck enum
+
+        GroupOfCards initDeck = new GroupOfCards();
+        
+        // initDeck contains all 52 card objects in Deck enum
+        for (int i = 0; i < deck.length ; i++) {
+            WarCard card = new WarCard(i);
+            initDeck.cards.add(card);
+        }
+        
+        System.out.println(initDeck.getSize()); // 52
+        
+        // checking : ordered 52 cards
+        for (int i = 0; i < initDeck.getSize(); i++) {
+            System.out.println(initDeck.getCards().get(i));  
+        }
+
+        // shuffle
+        initDeck.shuffle();
+        System.out.println("------------------------------------");
+        
+        // checking : shuffled 52 cards
+        for (int i = 0; i < initDeck.getSize(); i++) {
+            System.out.println(initDeck.getCards().get(i));  
+        }
+        
+        // initialize player's inhand GroupOfCard object
+        GroupOfCards inhand1 = new GroupOfCards();
+        GroupOfCards inhand2 = new GroupOfCards();
+        
+
+        // assign random 26 cards to each inhand object
+        for (int i = 0; i < initDeck.getCards().size(); i += 2) {
+            inhand1.cards.add(initDeck.getCards().get(i));
+            inhand2.cards.add(initDeck.getCards().get(i + 1));
+        }
+        
+        // checking : player 1's inhand 
+        System.out.println("------------------------------------");
+        System.out.println("\nPlayer 1's card list. Total: " + inhand1.getSize());
+        for (int i = 0; i < inhand1.getSize(); i++) {
+            System.out.println(inhand1.getCards().get(i));  
+        }
+
+        // checking : player 2's inhand 
+        System.out.println("------------------------------------");
+        System.out.println("\nPlayer 2's card list. Total: " + inhand1.getSize());
+        for (int i = 0; i < inhand2.getSize(); i++) {
+            System.out.println(inhand2.getCards().get(i));  
+        }
+
+    } // close main method -> NEED TO REMOVE
 
 }//end class
